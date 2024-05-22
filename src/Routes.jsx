76f95ea592a792ce8d9d086b7297ffc08e25d327
Irfankhan20 <sipathan96@gmail.com>
2024-05-22 07:55:57 +0600

    basic setup done and folder structure create

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import Contacts from "./pages/contacts/Contacts";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+      },
+      {
+        path: "/contacts",
+        element: <Contacts></Contacts>,
+      },
+    ],
   },
 ]);
 
---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,9 @@
+const Products = () => {
+  return (
+    <div>
+      <h2>this is products page</h2>
+    </div>
+  );
+};
+
+export default Products;
