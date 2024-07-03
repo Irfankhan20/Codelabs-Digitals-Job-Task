4becbe6e3c63ed4a333b46380d39dcf55e4aa309
Irfankhan20 <sipathan96@gmail.com>
2024-07-03 20:35:54 +0600

    comprehensive care section complete

--- src/pages/home/ourServices/OurServices.jsx
+++ src/pages/home/ourServices/OurServices.jsx
@@ -1,3 +1,5 @@
+import { FaStar } from "react-icons/fa";
+
 const OurServices = () => {
   return (
     <section className="bg-[#E0E2E9] ">
@@ -8,7 +10,7 @@ const OurServices = () => {
             Comprehensive Care <br /> for Every Patient
           </h2>
           {/* 1st card  */}
-          <div className="w-[220px] h-[312px] bg-[#FBFBFB] border border-red-600 rounded-2xl pl-5">
+          <div className="w-[220px] h-[312px] bg-[#FBFBFB] rounded-2xl pl-5">
             <h2 className="text-4xl font-bold pt-5">90%</h2>
             <p className="py-2">
               Patient satisfaction <br /> rate, reflecting our commitment.
@@ -21,18 +23,55 @@ const OurServices = () => {
           </div>
 
           {/* 2nd card */}
-          <div className="w-[220px] h-[190px] bg-[#FBFBFB] border border-red-600 rounded-2xl pl-5">
+          <div className="w-[220px] h-[190px] bg-[#FFFFF5] rounded-2xl pl-5 relative">
             <h2 className="text-4xl font-bold pt-5 ">500+</h2>
-            <p className="py-2 ">Board-certified doctors</p>
+            <p className="pt-2">
+              Board-certified <br /> doctors
+            </p>
             <img
-              className="flex justify-end"
+              className="pl-28 absolute bottom-5"
               src="https://i.ibb.co/KGqJw62/Screenshot-2024-07-03-200216.png"
               alt=""
             />
           </div>
-          <div className="w-[220px] h-[162px] bg-[#FBFBFB] border border-red-600 rounded-2xl"></div>
-          <div className="w-[220px] h-[190px] bg-[#FBFBFB] border border-red-600 rounded-2xl"></div>
-          <div className="w-[220px] h-[312px] bg-[#FBFBFB] border border-red-600 rounded-2xl"></div>
+
+          {/* 3rd card */}
+          <div className="w-[220px] h-[162px] bg-[#FBFBFB] rounded-2xl pl-5">
+            <h2 className="text-4xl font-bold pt-5 flex gap-2">
+              4.8{" "}
+              <span className=" text-[#FFE03D]">
+                <FaStar />
+              </span>
+            </h2>
+            <p className="pt-2">Over 20,000 Patient</p>
+            <img
+              className="pt-3 "
+              src="https://i.ibb.co/w01W0n1/Screenshot-2024-07-03-202009.png"
+              alt=""
+            />
+          </div>
+
+          {/* 4th card */}
+          <div className="w-[220px] h-[190px] bg-[#FFFFF5] rounded-2xl relative pl-5">
+            <h2 className="text-4xl font-bold pt-5">$5000</h2>
+            <p className="pt-2">Money spend for poor patient</p>
+            <img
+              className="pl-[70px] absolute bottom-7 "
+              src="https://i.ibb.co/rv9cXLy/Screenshot-2024-07-03-202649.png"
+              alt=""
+            />
+          </div>
+
+          {/* 5th card */}
+          <div className="w-[220px] h-[312px] bg-[#FBFBFB] rounded-2xl pl-5">
+            <h2 className="text-4xl font-bold pt-5">50+</h2>
+            <p className="pt-2">Free lession video for patient</p>
+            <img
+              className=" pl-5 pt-10"
+              src="https://i.ibb.co/0r8dwVH/Screenshot-2024-07-03-203201.png"
+              alt=""
+            />
+          </div>
         </div>
       </div>
     </section>
